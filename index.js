1e7c933b8ed0498dc43cd9adc51e041d43392e62
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 06:22:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {'Italian':'Pasta','Mexican':'Burrito','Chinese':'Dumpling'})
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj.[key]
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
