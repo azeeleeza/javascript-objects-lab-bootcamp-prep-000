61ce66795adaca4a053c848aad92ef6f19709497
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 06:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object)
-  delete newObj.[key]
+  delete newObj.key
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
+  delete object.key
   return object
 }
